46873186752957138423b0de9361d6d4e802288b
xEHx <55247913+xEHx@users.noreply.github.com>
2019-10-03 11:42:42 +0700

    Update HackCat.sh

--- HackCat.sh
+++ HackCat.sh
@@ -1,62 +1,72 @@
+#!/system/bin/sh
 clear
-echo "          *             *          "
+#ini warna
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+clear
+echo $me "          *             *          "
 sleep 0.03
-echo "          **            **         "
+echo $me"          **            **         "
 sleep 0.03
-echo "         ***  *******  ***         "
+echo $me"         ***  *******  ***         "
 sleep 0.03
-echo "         *****************         "
+echo $me"         *****************         "
 sleep 0.03
-echo "        *******************        "
+echo $me"        *******************        "
 sleep 0.03
-echo "       *********************       "
+echo $me"       *********************       "
 sleep 0.03
-echo "       *********************       "
+echo $me"       *********************       "
 sleep 0.03
-echo " *     *********************       "
+echo $me" *     *********************       "
 sleep 0.03
-echo "****    *******************        "
+echo $me"****    *******************        "
 sleep 0.03
-echo " ***      ***************          "
+echo $me" ***      ***************          "
 sleep 0.03
-echo "  ****        ******               "
+echo $me"  ****        ******               "
 sleep 0.03
-echo "   ****     ********               "
+echo $me"   ****     ********               "
 sleep 0.03
-echo "   ****    *********               "
+echo $me"   ****    *********               "
 sleep 0.03
-echo "   ****   ***********              "
+echo $me"   ****   ***********              "
 sleep 0.03
-echo "   ******************              "
+echo $me"   ******************              "
 sleep 0.03
-echo "    ****************               "
+echo $me"    ****************               "
 sleep 0.03
-echo "      ************                 "
+echo $me"      ************                 "
 sleep 1
-figlet HackCat
+figlet $me HackCat
 
-echo "___________________________________"
-echo "               HackCat TEAM "
+echo $me "___________________________________"
+echo $me "               HackCat TEAM "
 echo
-echo "CAPTAIN       : xEHx"
-echo "Co-Captain    : xGGx"
-echo "Member        : -PhantomThief"
-echo "                -N00B"
-echo "Website       : https://h4ckcat.000webhostapp.com"
-echo "___________________________________"
-echo "WELCOME TO HackCat !WHAT IS YOUR NAME?"
+echo $me "CAPTAIN       : xEHx"
+echo $me "Co-Captain    : xGGx"
+echo $me"Member        : -PhantomThief"
+echo $me "                -N00B"
+echo $me "Website       : https://h4ckcat.000webhostapp.com"
+echo $me "___________________________________"
+echo $me "WELCOME TO HackCat !WHAT IS YOUR NAME?"
 read -p"---->>>" nick;
-figlet $nick
-echo WELCOME $nick "!"
-echo "Please choose script!"
-echo "1.xEHx Tools"
+figlet $me $nick
+echo $me WELCOME $nick "!"
+echo $me "Please choose script!"
+echo $me "1.xEHx Tools"
 read nomor
 if [ $nomor = "1" ]; then
 clear
 sleep 1
-figlet xEHx Script
+figlet $me xEHx Script
 sleep 1
-echo "Loading xEHx Script...."
+echo $me "Loading xEHx Script...."
 git clone https://github.com/xEHx/Tools-v1
 cd Tools-v1
 sh Tools.sh
